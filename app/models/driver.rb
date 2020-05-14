c30d989bbee4328dbe602b81b57be6f39cd81918
GlassWall <karthikravirsb@gmail.com>
2020-05-14 23:03:13 +0530

    add driver model validations

---
+++ app/models/driver.rb
@@ -0,0 +1,15 @@
+class Driver < ApplicationRecord
+  attr_accessor :update_location
+  validates :name, :email, :phone_number, :license_number, :car_number, presence: true
+	validates_uniqueness_of :email, :phone_number, :license_number,:car_number
+  validates_length_of :phone_number, is: 10
+  validates :latitude, :longitude, presence: true, if: :update_location?
+  
+  
+  private
+
+  def update_location?
+    byebug
+    update_location
+  end
+end
