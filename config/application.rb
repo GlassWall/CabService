24fad5ac2cf763279f6b999025f20c1ecabcda67
GlassWall <karthikravirsb@gmail.com>
2020-05-16 16:12:48 +0530

    in memory implementation

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,6 @@ module Restapi
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
-    # ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
-    # ActiveRecord::Schema.verbose = false
-    # eval(`cat #{Rails.root.to_s}/db/schema.rb | sed 's/,[^:]*: :serial\//g'`)
-    # puts `bin/rails db:migrate`
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
