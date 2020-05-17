703278d96c68624b75ed2da694b6d754e2a83017
GlassWall <karthikravirsb@gmail.com>
2020-05-17 08:47:06 +0530

    Revert "Revert "in memory implementation""
    This reverts commit 2d0185b2da5052b33ec2b8b55cdb8134545cd40b.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+  load "#{Rails.root}/db/schema.rb"
 end
